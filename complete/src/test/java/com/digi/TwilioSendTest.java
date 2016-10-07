c060f61f68a1ff998ff8489ee428fdbffba20374
tymoshenkol <tymoshenkol@digicode.net>
2016-10-07 14:59:59 +0300

    test: send message to admin

--- complete/src/test/java/com/digi/TwilioSendTest.java
+++ complete/src/test/java/com/digi/TwilioSendTest.java
@@ -1,23 +1,16 @@
 package com.digi;
 
-import com.digi.entity.request.AccountToVerify;
-import com.digi.util.JsonUtil;
 import org.hamcrest.core.IsNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 
-
-import static io.jsonwebtoken.lang.Strings.hasLength;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
