d75fe33d343adfca70807f25afe15c3abbbecd70
tymoshenkol <tymoshenkol@digicode.net>
2016-10-20 19:10:08 +0300

    back to master

--- complete/src/main/java/com/digi/Application.java
+++ complete/src/main/java/com/digi/Application.java
@@ -1,11 +1,13 @@
 package com.digi;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -17,11 +19,6 @@ public class Application {
 	public static void main (String[] args) {
 		log.debug("args: {}", args);
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate () {
-		return new RestTemplate();
 	}
 
 }
